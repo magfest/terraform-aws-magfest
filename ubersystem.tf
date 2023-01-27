26aafce4af9fbba970e2c34f892daab644f2a162
Mark Murnane <mark@hackafe.net>
2023-01-26 23:47:40 -0500

    Adding dependency on RDS

--- ubersystem.tf
+++ ubersystem.tf
@@ -31,4 +31,7 @@ module "stock2023" {
     db_password             = aws_secretsmanager_secret_version.password.secret_string
     uber_db_name            = "stock2023"
     uber_db_username        = "stock2023"
+    depends_on              = [
+      aws_db_instance.uber
+    ]
 }
