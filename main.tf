36e2ddadfe99bc3604e9cabce5f42eeb2f82bad8
Mark Murnane <mark@hackafe.net>
2023-01-27 02:57:29 -0500

    Fixing EFS sg

--- main.tf
+++ main.tf
@@ -175,6 +175,35 @@ resource "aws_security_group" "uber_rds" {
   }
 }
 
+resource "aws_security_group" "uber_efs" {
+  name        = "uber_efs"
+  description = "Allow access to EFS"
+  vpc_id      = aws_vpc.uber.id
+
+  ingress {
+    description      = "EFS"
+    from_port        = 2049
+    to_port          = 2049
+    protocol         = "tcp"
+    cidr_blocks      = [
+        aws_subnet.primary.cidr_block,
+        aws_subnet.secondary.cidr_block
+    ]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Ubersystem EFS"
+  }
+}
+
 # -------------------------------------------------------------------
 # Subnets
 # -------------------------------------------------------------------
@@ -399,13 +428,19 @@ resource "aws_efs_file_system" "ubersystem_static" {
 }
 
 resource "aws_efs_mount_target" "primary" {
-  file_system_id = aws_efs_file_system.ubersystem_static.id
-  subnet_id      = aws_subnet.primary.id
+  file_system_id  = aws_efs_file_system.ubersystem_static.id
+  subnet_id       = aws_subnet.primary.id
+  security_groups = [
+    aws_security_group.uber_efs.id
+  ]
 }
 
 resource "aws_efs_mount_target" "secondary" {
-  file_system_id = aws_efs_file_system.ubersystem_static.id
-  subnet_id      = aws_subnet.secondary.id
+  file_system_id  = aws_efs_file_system.ubersystem_static.id
+  subnet_id       = aws_subnet.secondary.id
+  security_groups = [
+    aws_security_group.uber_efs.id
+  ]
 }
 
 # -------------------------------------------------------------------
