f8c0604614baf903d97f2539f00847094890406b
Mark Murnane <mark@hackafe.net>
2023-02-05 12:36:17 -0500

    Passing layout to uberserver-ecs

--- ubersystem.tf
+++ ubersystem.tf
@@ -31,6 +31,7 @@ module "uberserver-ecs" {
     efs_dir                 = lookup(each.value, "efs_dir", join("", ["/", each.key]))
     uber_db_name            = lookup(each.value, "uber_db_name", each.key)
     uber_db_username        = lookup(each.value, "uber_db_username", each.key)
+    layout                  = lookup(each.value, "layout", "scalable")
     
     web_count               = lookup(each.value, "web_count", 1)
     web_cpu                 = lookup(each.value, "web_cpu", 256)
