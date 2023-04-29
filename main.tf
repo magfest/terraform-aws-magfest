41fcfdcee1029de5e67b084029de7b5882c2ac2b
Mark Murnane <mark@hackafe.net>
2023-04-29 00:17:34 -0400

    Adding capacity provider

--- main.tf
+++ main.tf
@@ -545,6 +545,27 @@ resource "aws_autoscaling_group" "ecs_cluster" {
     aws_subnet.primary.id,
     aws_subnet.secondary.id
   ]
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = true
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_ecs_capacity_provider" "ec2_cluster" {
+  name = "EC2"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs_cluster.arn
+    managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 2
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 90
+    }
+  }
 }
 
 # -------------------------------------------------------------------
