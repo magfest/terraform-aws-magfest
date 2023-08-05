e5edf291cfdf1e0d166f7a2f4b87ddbf6c118d97
Mark Murnane <mark@hackafe.net>
2023-08-05 19:40:25 -0400

    Fixing cloudfront arguments

--- cloudfront.tf
+++ cloudfront.tf
@@ -61,6 +61,7 @@ resource "aws_cloudfront_distribution" "ubersystem" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.uber.arn
+    ssl_support_method  = "sni-only"
   }
 
   restrictions {
