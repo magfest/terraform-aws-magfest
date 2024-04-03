df9310af1f26b2d5bf17bc0f977800e4bc044fb9
Mark Murnane <mark@hackafe.net>
2024-04-02 20:22:46 -0400

    Fixing vars

--- modules/ubersystem-ecs/main.tf
+++ modules/ubersystem-ecs/main.tf
@@ -43,8 +43,8 @@ resource "aws_elasticache_cluster" "redis" {
   parameter_group_name = "default.redis7"
   engine_version       = "7.0"
   port                 = 6379
-  security_group_ids   = [aws_security_group.uber_redis.id]
-  subnet_group_name    = aws_elasticache_subnet_group.uber_subnets.name
+  security_group_ids   = [var.redis_sg_id]
+  subnet_group_name    = var.redis_subnet_group_name
 }
 
 # -------------------------------------------------------------------
