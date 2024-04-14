6a72cbc134a102d4008cea0d2ff959976929a9a0
Jason Spriggs <jason@jasonspriggs.com>
2024-04-14 14:49:59 -0400

    I dont think this is right, but we'll see!

--- datadog.tf
+++ datadog.tf
@@ -13,7 +13,7 @@ resource "aws_ssm_document" "datadog_install" {
         "name": "install",
         "inputs": {
           "runCommand": [
-            "DD_API_KEY=$(aws secretsmanager get-secret-value --secret-id ${aws_secretsmanager_secret.datadog_api_key.name}) DD_SITE="datadoghq.com" bash -c \"$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)\""
+            "DD_API_KEY=$(aws secretsmanager get-secret-value --secret-id ${aws_secretsmanager_secret.datadog_api_key[0].name}) DD_SITE="datadoghq.com" bash -c \"$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)\""
           ]
         }
       }
@@ -21,7 +21,7 @@ resource "aws_ssm_document" "datadog_install" {
   }
   EOF
 
-  depends_on = [ aws_secretsmanager_secret_version.datadog_api_key ]
+  depends_on = [ aws_secretsmanager_secret_version.datadog_api_key[0] ]
   count = var.datadog_api_key != null ? 0 : 1
 }
 
@@ -32,7 +32,7 @@ resource "aws_secretsmanager_secret" "datadog_api_key" {
 }
 
 resource "aws_secretsmanager_secret_version" "datadog_api_key" {
-  secret_id     = aws_secretsmanager_secret.datadog_api_key.id
+  secret_id     = aws_secretsmanager_secret.datadog_api_key[0].id
   secret_string = var.datadog_api_key
 
   count = var.datadog_api_key != null ? 0 : 1
